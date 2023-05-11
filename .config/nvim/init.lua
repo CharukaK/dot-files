3c479c22cc8f14372e81af3a44f5b3f168cd2cfe
CharukaK <charukakarunanayake@gmail.com>
2023-05-11 09:53:18 +0530

    Update vim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -303,11 +303,12 @@ vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { de
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
+
 -- -- split navigation
--- vim.keymap.set("n", "<C-h>", "<C-w>h");
--- vim.keymap.set("n", "<C-j>", "<C-w>j");
--- vim.keymap.set("n", "<C-k>", "<C-w>k");
--- vim.keymap.set("n", "<C-l>", "<C-w>l");
+vim.keymap.set("n", "<A-h>", "<C-w>h");
+vim.keymap.set("n", "<A-j>", "<C-w>j");
+vim.keymap.set("n", "<A-k>", "<C-w>k");
+vim.keymap.set("n", "<A-l>", "<C-w>l");
 
 -- set yanking configuration (copy/paste)
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
