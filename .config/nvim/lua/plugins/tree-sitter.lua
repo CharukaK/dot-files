6d20080b7dbe1d9d7cae6311144c1521f91b06e5
CharukaK <charukakarunanayake@gmail.com>
2024-05-21 09:43:28 +0530

    Update diagnostic keymap

--- .config/nvim/lua/plugins/tree-sitter.lua
+++ .config/nvim/lua/plugins/tree-sitter.lua
@@ -68,7 +68,12 @@ return {
                         ['<leader>A'] = '@parameter.inner',
                     },
                 },
-            },
+            }
         })
+        -- Diagnostic keymaps
+        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+        vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+        vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+        vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
     end
 }
