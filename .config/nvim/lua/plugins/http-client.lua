e53629ee90866b26638cecf1a2cea84dd11ca966
Charuka Karunanayake <charukakarunanayake@gmail.com>
2024-09-21 11:59:56 +0530

    Update rest client config

--- .config/nvim/lua/plugins/http-client.lua
+++ .config/nvim/lua/plugins/http-client.lua
@@ -1,4 +1,7 @@
 return {
     "rest-nvim/rest.nvim",
     dependencies = { 'nvim-neotest/nvim-nio' },
+    config = function ()
+        vim.keymap.set('n', '<leader>rr', ":Rest run<CR>")
+    end
 }
