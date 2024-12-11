766f2f0c360a15be923b6d5779937c506e5bd83f
CharukaK <charukakarunanayake@gmail.com>
2024-12-11 22:13:56 +0530

    Change status line mode indicator

--- .config/nvim/lua/plugins/status-line.lua
+++ .config/nvim/lua/plugins/status-line.lua
@@ -1,3 +1,12 @@
+-- return {
+--     'echasnovski/mini.statusline',
+--     version = '*',
+--     config = function()
+--         local statusline = require 'mini.statusline'
+--         statusline.setup { use_icons = true }
+--     end
+-- }
+
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
@@ -8,10 +17,13 @@ return {
                 section_seperators = "|"
             },
             sections = {
+                lualine_a = {
+                    { 'mode', fmt = function(str) return str:sub(1, 1) end }
+                },
                 lualine_c = {
                     {
                         'filename',
-                        path=1
+                        path = 1
                     }
                 }
             }
