3271d3f7080888af4ea7ea90fc0c87f54c398f05
Charuka Karunanayake <charukakarunanayake@gmail.com>
2025-01-21 20:50:06 +0530

    Add shortcuts for split in mini nvim

--- .config/nvim/lua/plugins/file-tree.lua
+++ .config/nvim/lua/plugins/file-tree.lua
@@ -38,9 +38,30 @@ return {
             minifile.open(file_dir, false)
         end
 
+        local map_split = function(buf_id, lhs, direction)
+            local rhs = function()
+                -- Make new window and set it as target
+                local cur_target = MiniFiles.get_explorer_state().target_window
+                local new_target = vim.api.nvim_win_call(cur_target, function()
+                    vim.cmd(direction .. ' split')
+                    return vim.api.nvim_get_current_win()
+                end)
+
+                MiniFiles.set_target_window(new_target)
+
+                -- This intentionally doesn't act on file under cursor in favor of
+                -- explicit "go in" action (`l` / `L`). To immediately open file,
+                -- add appropriate `MiniFiles.go_in()` call instead of this comment.
+            end
+
+            -- Adding `desc` will result into `show_help` entries
+            local desc = 'Split ' .. direction
+            vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = desc })
+        end
+
         vim.api.nvim_create_autocmd('User', {
             pattern = 'MiniFilesBufferCreate',
-            callback = function()
+            callback = function(args)
                 if vim.fn.argc() == 1 and vim.fn.isdirectory(vim.fn.argv(0)) == 1 then
                     local arg0 = vim.fn.argv(0)
 
@@ -50,6 +71,11 @@ return {
 
                     vim.fn.chdir(arg0)
                 end
+
+                local buf_id = args.data.buf_id
+                -- split function
+                map_split(buf_id, '<C-s>', 'belowright horizontal')
+                map_split(buf_id, '<C-v>', 'belowright vertical')
             end
         })
 
