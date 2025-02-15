83859bfa0902aad500a9f8a2619d0f64c1abfd46
Charuka Karunanayake <charukakarunanayake@gmail.com>
2025-02-15 22:38:21 +0530

    Switch to catpuccin theme

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,22 +1,22 @@
--- return {
---     "catppuccin/nvim",
---     name = "catppuccin",
---     priority = 1000,
---     config = function()
---         -- theme setup
---         require("catppuccin").setup({
---             flavor = "mocha",
---         })
---         vim.cmd.colorscheme "catppuccin"
---     end
--- }
 return {
-    "folke/tokyonight.nvim",
-    lazy = false,
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
-    opts = {},
     config = function()
-        require("tokyonight.theme").setup()
-        vim.cmd[[colorscheme tokyonight-night]]
+        -- theme setup
+        require("catppuccin").setup({
+            flavor = "mocha",
+        })
+        vim.cmd.colorscheme "catppuccin"
     end
 }
+-- return {
+--     "folke/tokyonight.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     opts = {},
+--     config = function()
+--         require("tokyonight.theme").setup()
+--         vim.cmd[[colorscheme tokyonight-night]]
+--     end
+-- }
